d655dd8747cd283b4006293fdc33a7267a1ff1db
Ranielli Montagna <raniellimontagna@hotmail.com>
2025-04-29 13:46:16 -0300

    feat: add jest

---
+++ src/app/__tests__/app.spec.tsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react-native'
+
+import { App } from '../app'
+
+describe('App', () => {
+  it('should render the app', () => {
+    const { getByText } = render(<App />)
+
+    expect(getByText('Welcome!')).toBeTruthy()
+  })
+})
