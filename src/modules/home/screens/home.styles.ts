5668da5a21195eb6044de9b57adba68be4e51226
Ranielli Montagna <raniellimontagna@hotmail.com>
2025-05-20 08:15:15 -0300

    style: update padding and font size formatting in home styles

--- src/modules/home/screens/home.styles.ts
+++ src/modules/home/screens/home.styles.ts
@@ -5,23 +5,23 @@ export const HomeContainer = styled.View`
   justify-content: center;
   align-items: center;
   background-color: ${props => props.theme.colors.background};
-  padding: ${props => props.theme.spacing[4]}px;
+  padding: ${props => `${props.theme.spacing[4]}px`};
 `
 
 export const Title = styled.Text`
-  font-size: ${props => props.theme.typography.fontSize['2xl']}px;
+  font-size: ${props => `${props.theme.typography.fontSize.xl}px`};
   color: ${props => props.theme.colors.text};
-  margin-bottom: ${props => props.theme.spacing[4]}px;
+  margin-bottom: ${props => `${props.theme.spacing[4]}px`};
 `
 
 export const ToggleButton = styled.TouchableOpacity`
   background-color: ${props => props.theme.colors.primary};
-  padding: ${props => props.theme.spacing[3]}px
-    ${props => props.theme.spacing[6]}px;
-  border-radius: ${props => props.theme.radius.md}px;
+  padding: ${props =>
+    `${props.theme.spacing[2]}px ${props.theme.spacing[4]}px`};
+  border-radius: ${props => `${props.theme.spacing[2]}px`};
 `
 
 export const ButtonText = styled.Text`
   color: white;
-  font-size: ${props => props.theme.typography.fontSize.md}px;
+  font-size: ${props => `${props.theme.typography.fontSize.md}px`};
 `
